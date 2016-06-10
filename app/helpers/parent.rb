30328fb81764a7a633889dd84e42ecf58915c0d5
José Mario López <jmlb@Joses-MacBook-Pro.local>
2016-06-09 21:00:59 -0600

    Created parent class for polymorphism

---
+++ app/helpers/parent.rb
@@ -0,0 +1,15 @@
+module Parent
+	class Parent
+		attr_accessor :states, :alphabet, :start, :accept, :transitions
+		def initialize(params={})
+      		yaml = {}
+      		yaml = YAML::load_file(params[:file]) if params.has_key? :file
+      		@states = yaml['states'] || params[:states]
+      		@alphabet = yaml['alphabet'] || params[:alphabet]
+      		@start = yaml['start'] || params[:start]
+      		@accept = yaml['accept'] || params[:accept]
+      		@transitions = yaml['transitions'] || params[:transitions]
+      		@transitions = Hash.keys_to_strings(@transitions)
+    	end
+	end
+end
