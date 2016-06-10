7d329971855bfad0dbc9d7aa39f6f5bd7702f578
Jorge Caballero <jorge@2build.it>
2016-06-09 21:38:36 -0600

    Initializing TM

--- app/helpers/tm_helper.rb
+++ app/helpers/tm_helper.rb
@@ -1,11 +1,18 @@
 module TmHelper
-    class TM < Parent::Parent
-        def initialize(elements={})
-            
-        end
-    end
+	class TM < Parent::Parent
+		def initialize(elements={})
+			yaml = {}
+			yaml = YAML::load_file(elements[:file]) if elements.has_key? :file
+			super(elements)
+			@inputAlphabet = elements[:inputAlphabet] || yaml['inputAlphabet']
+			@tapeAlphabet = elements[:tapeAlphabet] || yaml['tapeAlphabet']
+			@tape = Tape.new
+			@accept = false
+			@reject = false
+		end
+	end
 
-    class TMTape
-        
-    end
+	class TMTape
+
+	end
 end
