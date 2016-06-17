2162b87d8bb513c8594ed9470ce895b4181847d5
Jorge Caballero <jorge@2build.it>
2016-06-16 21:19:49 -0600

    Work

--- app/controllers/dfa_controller.rb
+++ app/controllers/dfa_controller.rb
@@ -1,2 +1,24 @@
+require 'json'
+
 class DfaController < ApplicationController
+    def index
+        @dfa = DfaHelper::DFA.new 
+    end
+
+    def compute
+        hash = dfa_params
+        hash = JSON.parse(hash) if hash.is_a?(String)
+
+        @dfa = DfaHelper::DFA.new()     
+        @dfa.states = hash['states'].split(',')
+        @dfa.alphabet = hash['alphabet'].split(/\s*,\s*/)
+        @dfa.start = hash['start']
+        @dfa.accept = hash['accept'].split(/\s*,\s*/)
+        @dfa.transitions = {"q1"=>{"0"=>"q2", "1"=>"q1"}, "q2"=>{"0"=>"q3", "1"=>"q1"}, "q3"=>{"0"=>"q3", "1"=>"q4"}, "q4"=>{"0"=>"q4", "1"=>"q4"}}
+    end
+
+    private
+        def dfa_params
+          params.permit(:states, :alphabet, :start, :accept, :transitions)
+        end
 end
--- app/helpers/dfa_helper.rb
+++ app/helpers/dfa_helper.rb
@@ -11,12 +11,20 @@ module DfaHelper
 
 		def consume(input)
 			head = @start.to_s
-			input.each_char { |symbol| head = @transitions[head][symbol] }
+			input.each_char { 
+                |symbol| head = @transitions[head][symbol]
+            }
 			accept = is_accept_state? head
 			resp = {
 				input: input,
 				accept: accept,
-				head: head 
+				head: head,
+                movements: ['q1', 'q2', 'q1'],
+                states: @states,
+                alphabet: @alphabet,
+                start: @start,
+                accept_state: @accept,
+                transitions: @transitions
 			}
 			resp
 		end
