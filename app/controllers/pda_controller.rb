75bc3e9fa8e6e9d5e04ef14fda87ec1cfd6deac3
pejuan <jose.pejuan@unitec.edu>
2016-06-16 21:57:51 -0600

    Pda controller

--- app/controllers/pda_controller.rb
+++ app/controllers/pda_controller.rb
@@ -1,2 +1,36 @@
+require 'json'
 class PdaController < ApplicationController
+
+	def index
+        @pda = PdaHelper::DFA.new 
+    end
+
+    def compute
+        hash = pda_params
+        hash = JSON.parse(hash) if hash.is_a?(String)
+
+        @pda = DfaHelper::DFA.new()     
+        @pda.states = hash['states'].split(',')
+        @pda.alphabet = hash['alphabet'].split(/\s*,\s*/)
+        @pda.start = hash['start']
+        @pda.accept = hash['accept'].split(/\s*,\s*/)
+        @pda.transitions = ""
+    end
+    def consume
+        hash = pda_params
+        hash = JSON.parse(hash) if hash.is_a?(String)
+        
+        @pda = DfaHelper::DFA.new()
+        @pda.states = hash['states'].split(' ')
+        @pda.alphabet = hash['alphabet'].split(' ')
+        @pda.start = hash['start']
+        @pda.accept = hash['accept'].split(' ')
+        @pda.transitions = ""
+        @compute = @pda.consume(hash['input_string'])
+    end
+
+    private
+        def pda_params
+          params.permit(:states, :alphabet, :start, :accept, :transitions, :input_string)
+        end
 end
