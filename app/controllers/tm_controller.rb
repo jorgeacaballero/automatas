182a2b7ecc36ec2c73189095f7b2535ecc264427
José Mario López <jmlb@2build.it>
2016-06-18 19:41:35 -0600

    Creando el tm_controller

--- app/controllers/tm_controller.rb
+++ app/controllers/tm_controller.rb
@@ -1,2 +1,40 @@
 class TmController < ApplicationController
+	def index
+        @tm = tmHelper::TM.new 
+    end
+
+    def compute
+        hash = tm_params
+        hash = JSON.parse(hash) if hash.is_a?(String)
+
+        @tm = tmHelper::TM.new()     
+        @tm.states = hash['states'].split(',')
+        @tm.alphabet = hash['alphabet'].split(/\s*,\s*/)
+        @tm.start = hash['start']
+        @tm.transitions = {"A"=>{"0"=>{"to"=>"A", "write"=>0, "move"=>"R"}, "1"=>{"to"=>"B", "write"=>1, "move"=>"R"}}, "B"=>{"1"=>{"to"=>"B", "write"=>1, "move"=>"R"}, "0"=>{"to"=>"C", "write"=>0, "move"=>"R"}}, "C"=>{"1"=>{"to"=>"ACCEPT", "write"=>1, "move"=>"R"}, "0"=>{"to"=>"A", "write"=>0, "move"=>"R"}}}
+        @tm.inputAlphabet = hash['inputAlphabet'].split(',')
+        @tm.tapeAlphabet = hash['tapeAlphabet'].split(',')
+
+    end
+
+    def consume
+        hash = tm_params
+        hash = JSON.parse(hash) if hash.is_a?(String)
+        
+        @tm = tmHelper::TM.new()
+        @tm.states = hash['states'].split(' ')
+        @tm.alphabet = hash['alphabet'].split(' ')
+        @tm.start = hash['start']
+        @tm.transitions = {"A"=>{"0"=>{"to"=>"A", "write"=>0, "move"=>"R"}, "1"=>{"to"=>"B", "write"=>1, "move"=>"R"}}, "B"=>{"1"=>{"to"=>"B", "write"=>1, "move"=>"R"}, "0"=>{"to"=>"C", "write"=>0, "move"=>"R"}}, "C"=>{"1"=>{"to"=>"ACCEPT", "write"=>1, "move"=>"R"}, "0"=>{"to"=>"A", "write"=>0, "move"=>"R"}}}
+        @tm.inputAlphabet = hash['inputAlphabet'].split(' ')
+        @tm.tapeAlphabet = hash['tapeAlphabet'].split(' ')
+
+        @compute = @tm.consume(hash['input_string'])
+
+    end
+
+    private
+        def tm_params
+          params.permit(:states, :alphabet, :start, :transitions, :input_string, :input_alphabet, :tape_alphabet)
+        end
 end
