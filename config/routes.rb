dc2d29c839d83fa51f05a8f20c69b806aadb4991
José Mario López <jmlb@2build.it>
2016-06-18 19:51:28 -0600

    creando las rutas para la tm

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
     get '/pda', to: 'pda#index', as: :pda
     post '/pda/compute', to: 'pda#compute', as: :pda_compute
     post '/pda/consume', to: 'pda#consume', as: :pda_consume
+
+    get '/tm', to: 'tm#index', as: :tm
+    post '/tm/compute', to: 'tm#compute', as: :tm_compute
+    post '/tm/consume', to: 'tm#consume', as: :tm_consume
   
 end
