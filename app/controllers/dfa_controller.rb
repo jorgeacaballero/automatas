2205fe0549ada0f83c080249530c7f6c06e2ef51
Jorge Caballero <j@caballeromurillo.com>
2016-06-21 03:47:04 +0000

    Implementando Cytoscape dibujado del dfa

--- app/controllers/dfa_controller.rb
+++ app/controllers/dfa_controller.rb
@@ -2,25 +2,65 @@ require 'json'
 
 class DfaController < ApplicationController
     def index
-        @dfa = DfaHelper::DFA.new 
+        @dfa = DfaHelper::DFA.new
     end
 
     def compute
         hash = dfa_params
         hash = JSON.parse(hash) if hash.is_a?(String)
 
-        @dfa = DfaHelper::DFA.new()     
+        @dfa = DfaHelper::DFA.new()
         @dfa.states = hash['states'].split(',')
         @dfa.alphabet = hash['alphabet'].split(/\s*,\s*/)
         @dfa.start = hash['start']
         @dfa.accept = hash['accept'].split(/\s*,\s*/)
-        @dfa.transitions = {"q1"=>{"0"=>"q2", "1"=>"q1"}, "q2"=>{"0"=>"q3", "1"=>"q1"}, "q3"=>{"0"=>"q3", "1"=>"q4"}, "q4"=>{"0"=>"q4", "1"=>"q4"}}
+        @dfa.transitions = {
+          "q1" => {
+            "0" => "q2",
+            "1" => "q1"
+          },
+          "q2" => {
+            "0" => "q3",
+            "1" => "q1"
+          },
+          "q3" => {
+            "0" => "q3",
+            "1" => "q4"
+          },
+          "q4" => {
+            "0" => "q4",
+            "1" => "q4"
+          }
+      }
+
+      nodes = []
+      edges = []
+
+      @dfa.states.each do |s|
+        n = { data: { id: s }}
+        nodes.push(n)
+      end
+
+      i = 1
+      @dfa.transitions.each do |keyt, valt|
+        valt.each do |key, val|
+          n = { data: { id: i.to_s, source: keyt, target: val, label: key}}
+          edges.push(n)
+          i = i+1
+        end
+      end
+
+      @churro = {
+          nodes: nodes,
+
+          edges: edges
+        }.to_json.html_safe
     end
 
     def consume
         hash = dfa_params
         hash = JSON.parse(hash) if hash.is_a?(String)
-        
+
         @dfa = DfaHelper::DFA.new()
         @dfa.states = hash['states'].split(' ')
         @dfa.alphabet = hash['alphabet'].split(' ')
