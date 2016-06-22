e550376debc77597e79b19f5df53ed6eb8d44453
José Mario López <jmlb@2build.it>
2016-06-21 20:04:30 -0600

    Fixes MASIVOS en todo TM

--- app/controllers/tm_controller.rb
+++ app/controllers/tm_controller.rb
@@ -7,9 +7,9 @@ class TmController < ApplicationController
         hash = tm_params
         hash = JSON.parse(hash) if hash.is_a?(String)
 
-        @tm = TmHelper::TM.new()     
+        @tm = TmHelper::TM.new
         @tm.states = hash['states'].split(',')
-        @tm.alphabet = hash['alphabet'].split(/\s*,\s*/)
+        #@tm.alphabet = hash['alphabet'].split(',')
         @tm.start = hash['start']
         @tm.transitions = {"A"=>{"0"=>{"to"=>"A", "write"=>0, "move"=>"R"}, "1"=>{"to"=>"B", "write"=>1, "move"=>"R"}}, "B"=>{"1"=>{"to"=>"B", "write"=>1, "move"=>"R"}, "0"=>{"to"=>"C", "write"=>0, "move"=>"R"}}, "C"=>{"1"=>{"to"=>"ACCEPT", "write"=>1, "move"=>"R"}, "0"=>{"to"=>"A", "write"=>0, "move"=>"R"}}}
         @tm.inputAlphabet = hash['inputAlphabet'].split(',')
@@ -50,15 +50,15 @@ class TmController < ApplicationController
         hash = tm_params
         hash = JSON.parse(hash) if hash.is_a?(String)
         
-        @tm = TmHelper::TM.new()
+        @tm = TmHelper::TM.new
         @tm.states = hash['states'].split(' ')
-        @tm.alphabet = hash['alphabet'].split(' ')
+        #@tm.alphabet = hash['alphabet'].split(' ')
         @tm.start = hash['start']
         @tm.transitions = {"A"=>{"0"=>{"to"=>"A", "write"=>0, "move"=>"R"}, "1"=>{"to"=>"B", "write"=>1, "move"=>"R"}}, "B"=>{"1"=>{"to"=>"B", "write"=>1, "move"=>"R"}, "0"=>{"to"=>"C", "write"=>0, "move"=>"R"}}, "C"=>{"1"=>{"to"=>"ACCEPT", "write"=>1, "move"=>"R"}, "0"=>{"to"=>"A", "write"=>0, "move"=>"R"}}}
         @tm.inputAlphabet = hash['inputAlphabet'].split(' ')
         @tm.tapeAlphabet = hash['tapeAlphabet'].split(' ')
 
-        @compute = @tm.consume(hash['input_string'])
+        @compute = @tm.feed(hash['input_string'])
 
     end
 
